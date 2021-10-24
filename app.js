165ace8994c4ac671140c4256c6a65d6ee77a385
lettuce <lettuce.lu@lettucelu.com>
2021-10-24 23:14:01 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const port = 3000
 const exphbs = require('express-handlebars')
 // const restaurantList = require('./restaurant.json')
@@ -16,6 +17,12 @@ const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
