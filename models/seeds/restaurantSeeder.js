746b496aa7f93b8c3e35912d0ba1953c1d13dd1b
lettuce <lettuce.lu@lettucelu.com>
2021-10-25 00:27:24 +0800

    recreate seed file

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,16 +1,94 @@
-const Rest = require('../restaurant')
-const restaurantList = require('../../restaurant.json')
 
-// mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
-// const db = mongoose.connection
-// db.on('error', () => {
-//   console.log('mongodb error!')
-// })
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const restaurantSchema = require('../restaurant')
+
+const initData = require('../../restaurant.json')
+
+const User = require('../user')
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
+
 const db = require('../../config/mongoose')
-db.once('open', () => {
-  // console.log('mongodb connected!')
-  restaurantList.results.forEach((restaurant) => {
-    Rest.create(restaurant)
-  })
-  console.log('done')
+const userArray = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678'
+  }
+  ,
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+
+]
+db.once('open', async () => {
+  for (let i = 0; i < userArray.length; i++) {
+    console.log(userArray[i].email)
+
+    let { email, password, name } = userArray[i]
+    User.findOne({ email })
+      .lean()
+      .then(async user => {
+        console.log(user)
+        if (user) {
+          console.log('有使用者')
+        } else {
+          console.log('準備創建使用者')
+          await bcrypt
+            .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+            .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+            .then(hash =>
+              User.create({
+                name,
+                email,
+                password: hash // 用雜湊值取代原本的使用者密碼
+              })
+            )
+            .then(async user => {
+              console.log('create succeed')
+              if (i === 0) {
+                for (let j = 0; j < 3; j++) {
+                  initData.results[j].userId = user._id
+                  await restaurantSchema.create(initData.results[j])
+                    .then(() => {
+                      console.log('創建資料成功')
+                    })
+                    .catch(err => {
+                      console.log(err)
+                    })
+                }
+              } else {
+                for (let j = 3; j < 6; j++) {
+                  initData.results[j].userId = user._id
+                  await restaurantSchema.create(initData.results[j])
+                    .then(() => {
+                      console.log('創建資料成功')
+                    })
+                    .catch(err => {
+                      console.log(err)
+                    })
+                }
+              }
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        }
+      })
+      .then(() => {
+        console.log('done.')
+        process.exit()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+
+  }
+
 })
